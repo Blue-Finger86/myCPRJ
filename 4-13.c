71dcbbdce2f30598fba03673f01a6f15147d96a0
SoyoungKim <126838903+Blue-Finger86@users.noreply.github.com>
2023-07-13 23:55:16 +0900

    230713

---
+++ 4-13.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main()
+{
+	int a = 10;
+
+	printf("%d를 오른쪽으로 1회 시프트하면 %d이다. \n", a, a>>1);
+	printf("%d를 오른쪽으로 2회 시프트하면 %d이다. \n", a, a>>2);
+	printf("%d를 오른쪽으로 3회 시프트하면 %d이다. \n", a, a>>3);
+	printf("%d를 오른쪽으로 4회 시프트하면 %d이다. \n", a, a>>4);
+}
+
